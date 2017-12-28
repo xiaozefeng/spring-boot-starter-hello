2e4a7709497e8206d0c25584daf68f18560d4c96
肖泽锋 <qq523107430@163.com>
2017-12-28 23:48:08 +0800

    自定义spring boot starter

---
+++ src/main/java/com/gz/learnspringboot/HelloServiceProperties.java
@@ -0,0 +1,22 @@
+package com.gz.learnspringboot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author xiaozefeng
+ */
+@ConfigurationProperties(prefix = "hello")
+public class HelloServiceProperties {
+    private static final String MSG = "world";
+
+    private String msg = MSG;
+
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
